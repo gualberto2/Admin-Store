029d13b78062d1bbbdf5229642436d0400876ab2
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-21 19:45:25 -0700

    created button comp, working on shopping cart

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+import { ShoppingBag } from "lucide-react";
+import Button from "@/components/ui/button";
+
 const NavbarActions = () => {
-  return <div className="ml-auto items-center flex gap-x-4">hello</div>;
+  return (
+    <div className="ml-auto items-center flex gap-x-4">
+      <Button>
+        <ShoppingBag size={20} color="white"></ShoppingBag>
+      </Button>
+    </div>
+  );
 };
 
 export default NavbarActions;
---
+++ components/ui/button.tsx
@@ -0,0 +1,23 @@
+import { forwardRef } from "react";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+  className,
+  children,
+  disabled,
+  type = "button",
+  ...props,
+}, ref)=>{
+    return(
+        <button className="" ref={ref}>
+{children}
+        </button>
+    )
+};
+
+Button.displayName= "Button"
+
+
+export default Button
