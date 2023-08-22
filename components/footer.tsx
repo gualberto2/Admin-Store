bd0e4e994aff8e09301b8464b6bde58d1ab93cd5
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-21 19:37:15 -0700

    provided root css classes

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="bg-white border-t">
+      <div className="py-10 mx-auto">
+        <p className="text-black text-center text-xs">
+          &copy: 2033 FakeStoreNamA, Inc. All rights reserved
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/navbar-actions.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const NavbarActions = () => {
+  return <div className="ml-auto items-center flex gap-x-4">hello</div>;
+};
+
+export default NavbarActions;
