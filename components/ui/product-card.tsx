1e4fd09efabdf5f4cd27a77207034f0a6bffa239
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-23 14:16:55 -0700

    provided even more styling

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -3,6 +3,8 @@
 import { Product } from "@/types";
 
 import Image from "next/image";
+import IconButton from "@/components/ui/icon-button";
+import { Expand, ShoppingCart } from "lucide-react";
 
 interface ProductCard {
   data: Product;
@@ -19,11 +21,27 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
           fill
           className="aspect-square object-cover rounded-md"
         />
-        <div className="opacity-0 group-hover:opacity-100">
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            <IconButton />
+            <IconButton
+              onClick={() => {}}
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconButton
+              onClick={() => {}}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
+            />
           </div>
         </div>
+      </div>
+      {/* Description */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* Price */}
+      <div className="flex items-center justify-between">
+        <Currency value={data?.price} />
       </div>
     </div>
   );
