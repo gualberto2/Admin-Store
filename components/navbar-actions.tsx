52fa818760e8c64f5851b54808fecb1be8ad46a1
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-22 20:10:12 -0700

    added hydration

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -2,12 +2,22 @@
 
 import { ShoppingBag } from "lucide-react";
 import Button from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
   return (
     <div className="ml-auto items-center flex gap-x-4">
-      <Button>
-        <ShoppingBag size={20} color="white"></ShoppingBag>
+      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag size={20} color="white" />
+        <span className="ml-2"></span>
       </Button>
     </div>
   );
