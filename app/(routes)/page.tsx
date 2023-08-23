e94f69bb2154203e4f8cc00cd4b5c96867c63912
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-22 21:46:33 -0700

    provided get products action

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import getBillboards from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboards("54162fba-7c4d-490e-804d-2e2afd65983b");
   return (
     <Container>
