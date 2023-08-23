0ae5222d77f59722f49d66f885782aa0f8cf9787
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-22 21:34:20 -0700

    added async fetch

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,12 +1,15 @@
+import getBillboard from "@/actions/get-billboards";
+import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
-const HomePage = () => {
+const HomePage = async () => {
+  const billboard = await getBillboard("54162fba-7c4d-490e-804d-2e2afd65983b");
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard />
+        <Billboard data={billboard} />
       </div>
     </Container>
   );
