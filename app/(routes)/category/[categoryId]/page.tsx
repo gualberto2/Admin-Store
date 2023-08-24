a9c2120fd81c04dd5f82edc5703f9da4cbef7417
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-23 22:48:34 -0700

    props added, constructing

---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,25 @@
+import getProducts from "@/actions/get-products";
+
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
+}) => {
+  const products = await getProducts({
+    categoryId: params.categoryId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
+  });
+  return <div>cat page</div>;
+};
+
+export default CategoryPage;
