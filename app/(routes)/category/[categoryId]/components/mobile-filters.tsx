c9dd565a9480a2ef3aed953f0af7b6b02ecc4ee6
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-24 14:16:06 -0700

    debugging

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import Button from "@/components/ui/button";
-import IconButton from "@/components/ui/icon-button";
-import { Size, Color } from "@/types";
-import { Dialog } from "@headlessui/react";
-import { Plus, X } from "lucide-react";
 import { useState } from "react";
+import { Plus, X } from "lucide-react";
+import { Dialog } from "@headlessui/react";
+
+import IconButton from "@/components/ui/icon-button";
+import Button from "@/components/ui/button";
+import { Color, Size } from "@/types";
+
 import Filter from "./filter";
 
 interface MobileFiltersProps {
@@ -32,17 +34,17 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({ sizes, colors }) => {
         className="relative z-40 lg:hidden"
         onClose={onClose}
       >
-        {/* Background */}
+        {/* Background color and opacity */}
         <div className="fixed inset-0 bg-black bg-opacity-25" />
-        {/* Dialog Position */}
+
+        {/* Dialog position */}
         <div className="fixed inset-0 z-40 flex">
-          <Dialog.Panel className="relative ml-auto flex h-full py-4 pb-6 shadow-xl w-full max-w-xs flex-col overflow-y-auto bg-white">
+          <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
             {/* Close button */}
             <div className="flex items-center justify-end px-4">
               <IconButton icon={<X size={15} />} onClick={onClose} />
             </div>
 
-            {/* Render Filters */}
             <div className="p-4">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
@@ -53,4 +55,5 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({ sizes, colors }) => {
     </>
   );
 };
+
 export default MobileFilters;
