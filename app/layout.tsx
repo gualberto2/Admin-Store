751c357e9ee022f31557e51be727f2a72c1848bb
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-26 09:48:41 -0700

    added toastprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import Navbar from "@/components/navbar";
 import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToastProvider />
         <ModalProvider />
         <Navbar />
         {children}
