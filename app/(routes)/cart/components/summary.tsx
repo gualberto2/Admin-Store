33234f3dd62db61d6781eac0ecfa4e839e29c5d7
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-27 22:13:45 -0700

    implemented new features

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -9,6 +9,35 @@ import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
 
 const Summary = () => {
+  const searchParams = useSearchParams();
+  const items = useCart((state) => state.items);
+  const removeAll = useCart((state) => state.removeAll);
+
+  useEffect(() => {
+    if (searchParams.get("success")) {
+      toast.success("Payment complete");
+      removeAll();
+    }
+
+    if (searchParams.get("cancelled")) {
+      toast.error("Something went wrong");
+    }
+  }, [searchParams, removeAll]);
+
+  const totalPrice = items.reduce((total, item) => {
+    return total + Number(item.price);
+  }, 0);
+
+  const onCheckout = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    );
+    window.location = response.data.url;
+  };
+
   return (
     <div
       className="mt-16
@@ -25,10 +54,12 @@ const Summary = () => {
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <div className="text-base font-medium text-gray-900">Order Total</div>
-          <Currency value={45} />
+          <Currency value={totalPrice} />
         </div>
       </div>
-      <Button className="w-full mt-6">Checkout</Button>
+      <Button onClick={onCheckout} className="w-full mt-6">
+        Checkout
+      </Button>
     </div>
   );
 };
