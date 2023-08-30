06ee184d934287bc4372a331cd13bea2b3728add
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-30 12:43:59 -0700

    ERMOVED TO TRY AND FIX

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <ToastProvider />
+        {/* <ToastProvider /> */}
         <ModalProvider />
         <Navbar />
         {children}
